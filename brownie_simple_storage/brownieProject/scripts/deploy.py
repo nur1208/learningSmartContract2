786dc51862610f7f99d172118c6f2395a5ddb585
nur <medo0o6665@gmail.com>
2022-04-30 17:28:46 +0800

    learning how to deploy contract to rinkeby test network using brownie and how to interact with a contract in brownie and learning about brownie console

--- brownie_simple_storage/brownieProject/scripts/deploy.py
+++ brownie_simple_storage/brownieProject/scripts/deploy.py
@@ -1,8 +1,8 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 # import os
 
 def deploy_simple_storage():
-    account = accounts[0]
+    # account = accounts[0]
     # print(account)
 
     # account = accounts.load("nur")
@@ -13,6 +13,7 @@ def deploy_simple_storage():
     # account = accounts.add(config["wallets"]["from_key"])
     # print(account)
 
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from":account})
     stored_value = simple_storage.retrieve()
     print(stored_value)
@@ -23,6 +24,11 @@ def deploy_simple_storage():
     updated_value = simple_storage.retrieve()
     print(updated_value)
 
+def get_account():
+    if(network.show_active() == "development"):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_simple_storage()
